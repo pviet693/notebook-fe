c55d3f96e4a9eb7e22daba44496240d06fb957b3
vietpham <pviet693@gmail.com>
2024-12-24 17:15:05 +0700

    add: github action

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import path from "path";
 import { defineConfig } from "vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
+import commonjs from "vite-plugin-commonjs";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [TanStackRouterVite(), react()],
+    plugins: [TanStackRouterVite(), react(), commonjs()],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src")
