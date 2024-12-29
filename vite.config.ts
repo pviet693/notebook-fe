1742c1d5f4c74b7eedc3f2d1c5cbf818c1625ca2
Phạm Văn Việt <47064221+pviet693@users.noreply.github.com>
2024-12-29 21:49:26 +0700

    chore: reverse changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,18 @@
 import path from "path";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import commonjs from "vite-plugin-commonjs";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd());
-
-    return {
-        base: env.VITE_BASE,
-        plugins: [TanStackRouterVite(), react(), commonjs()],
-        resolve: {
-            alias: {
-                "@": path.resolve(__dirname, "./src")
-            }
-        },
-        build: {
-            sourcemap: true
+export default defineConfig({
+    plugins: [TanStackRouterVite(), react(), commonjs()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src")
         }
-    };
+    },
+    build: {
+        sourcemap: true
+    }
 });
