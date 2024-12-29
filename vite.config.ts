82e7ada8790eade37d066b2300f4c6dc3792bebc
Phạm Văn Việt <47064221+pviet693@users.noreply.github.com>
2024-12-29 19:18:09 +0700

    chore: update base url for css, js

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,23 @@
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import commonjs from "vite-plugin-commonjs";
 
 // https://vite.dev/config/
-export default defineConfig({
-    base: "./dist",
-    plugins: [TanStackRouterVite(), react(), commonjs()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src")
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
+
+    return {
+        base: env.VITE_BASE,
+        plugins: [TanStackRouterVite(), react(), commonjs()],
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "./src")
+            }
+        },
+        build: {
+            sourcemap: true
         }
-    },
-    build: {
-        sourcemap: true
-    }
+    };
 });
