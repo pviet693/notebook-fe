8340408ad2d535cfd494c7623cf50d237c3cb116
vietpham <pviet693@gmail.com>
2024-12-31 10:06:37 +0700

    fix: update auth condition

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -70,7 +70,7 @@ export default function Navbar() {
                         </Button>
                     </Link>
 
-                    {isLoggedIn() ? (
+                    {isLoggedIn ? (
                         <>
                             <NotificationBell />
                             <DropdownMenu>
--- src/routes/_layout/_authenticated.tsx
+++ src/routes/_layout/_authenticated.tsx
@@ -3,8 +3,7 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 export const Route = createFileRoute("/_layout/_authenticated")({
     beforeLoad: async ({ context }) => {
         const { isLoggedIn } = context.auth;
-        console.log(context);
-        if (!isLoggedIn()) {
+        if (!isLoggedIn) {
             const redirectUri =
                 window.location.pathname + window.location.search;
 
